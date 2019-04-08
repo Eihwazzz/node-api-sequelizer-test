91c8da222ca72470d1947a08346aebdd9f100155
Brito <alberto.brito@capgemini.com>
2019-04-08 09:56:03 -0300

    Se cambia puerto para que no intercepte con las otras aplicaciones

--- src/libs/middlewares.js
+++ src/libs/middlewares.js
@@ -3,7 +3,7 @@ import express from 'express';
 module.exports = app => {
 
     // Configuraciones
-    app.set('port', process.env.PORT || 3003);
+    app.set('port', process.env.PORT || 3005);
 
     // middleware
     app.use(express.json());
